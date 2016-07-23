2e567425e327d3ce7d42a75d42ee424036f65976
Shifteh Sherafat <shifteh.sh@gmail.com>
2016-07-23 17:40:34 +0200

    xsquared

--- test/xsquared.py
+++ test/xsquared.py
@@ -1,4 +1,21 @@
 
 
-def f(x):
+def f2(x):
 	return x*x
+
+
+def f3(x):
+	x*x*x
+
+def f4(x):
+	x*x*x*x
+
+
+
+if name == "__main__":
+	from numpy import *
+	x = linspace(1, 10,1000)
+	xsquared = f2(x)
+	xcubed = f3(x)
+	xfourth = f4(x)
+	
